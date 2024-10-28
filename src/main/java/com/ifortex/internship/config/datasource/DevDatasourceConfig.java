46ed50c2f45aac96c151fd1f7f8a2d2011cc9638
AndreySemeynikov <andrey.semeynikov@ifortex.com>
2024-10-28 18:23:01 +0300

    EPS-5: define db connection, setup migration script, format sql script

---
+++ src/main/java/com/ifortex/internship/config/datasource/DevDatasourceConfig.java
@@ -0,0 +1,41 @@
+package com.ifortex.internship.config.datasource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DevDatasourceConfig implements DatasourceConfig {
+
+  private final ApplicationDatasourceProperties applicationProperties;
+
+  public DevDatasourceConfig(ApplicationDatasourceProperties applicationProperties) {
+    this.applicationProperties = applicationProperties;
+  }
+
+  @Override
+  @Bean
+  @Profile("dev")
+  public HikariDataSource dataSource() {
+    HikariConfig hikariConfig = new HikariConfig();
+
+    hikariConfig.setDriverClassName(applicationProperties.getDriver());
+    hikariConfig.setJdbcUrl(applicationProperties.getUrl());
+    hikariConfig.setUsername(applicationProperties.getUsername());
+    hikariConfig.setPassword(applicationProperties.getPassword());
+
+    hikariConfig.setPoolName("devSpringHikariCP");
+    hikariConfig.setAutoCommit(true);
+    hikariConfig.setMaximumPoolSize(20);
+
+    return new HikariDataSource(hikariConfig);
+  }
+
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(dataSource());
+  }
+}
