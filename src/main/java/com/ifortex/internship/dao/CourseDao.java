b732e464ef4e798034f1ad14c75539a6ceedd609
AndreySemeynikov <andrey.semeynikov@ifortex.com>
2024-11-05 12:59:43 +0300

    EPS-14: changed update function in CourseDao, implemented students delete/enrollments in a CourseDao

--- src/main/java/com/ifortex/internship/dao/CourseDao.java
+++ src/main/java/com/ifortex/internship/dao/CourseDao.java
@@ -2,11 +2,12 @@ package com.ifortex.internship.dao;
 
 import com.ifortex.internship.model.Course;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
  * Data Access Object (DAO) interface for managing {@link Course} entities. Provides methods to
- * perform CRUD operations on course data in the database.
+ * perform CRUD operations and manage course-student enrollments in the database.
  */
 public interface CourseDao {
 
@@ -33,11 +34,12 @@ public interface CourseDao {
   List<Course> findAll();
 
   /**
-   * Updates an existing course record in the database.
+   * Updates an existing course record in the database with specified field-value mappings.
    *
-   * @param course the course entity with updated information.
+   * @param id the id of the course to update.
+   * @param updates a map of field names and their new values for the course update.
    */
-  void update(Course course);
+  void update(long id, Map<String, Object> updates);
 
   /**
    * Deletes a course from the database by its id.
@@ -45,4 +47,37 @@ public interface CourseDao {
    * @param id the id of the course to delete.
    */
   void delete(long id);
+
+  /**
+   * Enrolls a student in a course by adding an entry to the course-student association table.
+   *
+   * @param courseId the id of the course in which to enroll the student.
+   * @param studentId the id of the student to enroll in the course.
+   */
+  void enrollStudentInCourse(long courseId, long studentId);
+
+  /**
+   * Removes a student from a course by deleting the corresponding entry from the course-student
+   * association table.
+   *
+   * @param courseId the id of the course from which to remove the student.
+   * @param studentId the id of the student to remove from the course.
+   */
+  void removeStudentFromCourse(long courseId, Long studentId);
+
+  /**
+   * Enrolls multiple students in a course using batch processing.
+   *
+   * @param courseId the id of the course in which to enroll students.
+   * @param studentIds a list of student IDs to enroll in the course.
+   */
+  void batchEnrollStudents(long courseId, List<Long> studentIds);
+
+  /**
+   * Removes multiple students from a course using batch processing.
+   *
+   * @param courseId the id of the course from which to remove students.
+   * @param studentIds a list of student id to remove from the course.
+   */
+  void batchRemoveStudents(long courseId, List<Long> studentIds);
 }
--- src/main/java/com/ifortex/internship/dao/impl/CourseDaoImpl.java
+++ src/main/java/com/ifortex/internship/dao/impl/CourseDaoImpl.java
@@ -1,35 +1,32 @@
 package com.ifortex.internship.dao.impl;
 
 import com.ifortex.internship.dao.CourseDao;
+import com.ifortex.internship.dao.mapper.CourseWithStudentsExtractor;
 import com.ifortex.internship.model.Course;
-import com.ifortex.internship.model.Student;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CourseDaoImpl implements CourseDao {
   private final JdbcTemplate jdbcTemplate;
-  private final RowMapper<Course> courseRowMapper;
-  private final RowMapper<Student> studentRowMapper;
+  private final ResultSetExtractor<List<Course>> courseWithStudentsExtractor;
 
   public CourseDaoImpl(
-      JdbcTemplate jdbcTemplate,
-      RowMapper<Course> courseRowMapper,
-      RowMapper<Student> studentRowMapper) {
+      JdbcTemplate jdbcTemplate, CourseWithStudentsExtractor courseWithStudentsExtractor) {
     this.jdbcTemplate = jdbcTemplate;
-    this.courseRowMapper = courseRowMapper;
-    this.studentRowMapper = studentRowMapper;
+    this.courseWithStudentsExtractor = courseWithStudentsExtractor;
   }
 
   @Override
   public void create(Course course) {
     String sql =
-        "INSERT INTO course (name, description, price, duration, start_date, last_update_date, course_status) VALUES (?, ?, ?, ?, ?, ?, ?::course_status)";
+        "INSERT INTO course (name, description, price, duration, start_date, last_update_date, course_status)"
+            + "VALUES (?, ?, ?, ?, ?, ?, ?::course_status)";
     jdbcTemplate.update(
         sql,
         course.getName(),
@@ -43,57 +40,105 @@ public class CourseDaoImpl implements CourseDao {
 
   @Override
   public Optional<Course> find(long id) {
-    String sql = "SELECT * FROM course WHERE id = ?";
-    Optional<Course> course = jdbcTemplate.query(sql, courseRowMapper, id).stream().findFirst();
+    String sql =
+        "SELECT c.id AS course_id, "
+            + "c.name AS course_name, "
+            + "c.description AS course_description, "
+            + "c.price AS course_price, "
+            + "c.duration AS course_duration, "
+            + "c.start_date AS course_start_date, "
+            + "c.last_update_date AS course_last_update_date, "
+            + "c.course_status AS course_status, "
+            + "s.id AS student_id, "
+            + "s.name AS student_name "
+            + "FROM course c "
+            + "LEFT JOIN course_student cs ON c.id = cs.course_id "
+            + "LEFT JOIN student s ON cs.student_id = s.id "
+            + "WHERE c.id = ?";
 
-    Set<Student> students =
-        Set.copyOf(
-            jdbcTemplate.query(
-                "SELECT s.* FROM student s JOIN course_student cs ON s.id = cs.student_id WHERE cs.course_id = ?",
-                studentRowMapper,
-                id));
-
-    return course.map(
-        c -> {
-          c.setStudentSet(students);
-          return c;
-        });
+    List<Course> courses = jdbcTemplate.query(sql, courseWithStudentsExtractor, id);
+    return courses.stream().findFirst();
   }
 
   @Override
   public List<Course> findAll() {
-    String sql = "SELECT * FROM course";
-    List<Course> courses = jdbcTemplate.query(sql, courseRowMapper);
+    String sql =
+        "SELECT c.id AS course_id, "
+            + "c.name AS course_name, "
+            + "c.description AS course_description, "
+            + "c.price AS course_price, "
+            + "c.duration AS course_duration, "
+            + "c.start_date AS course_start_date, "
+            + "c.last_update_date AS course_last_update_date, "
+            + "c.course_status AS course_status, "
+            + "s.id AS student_id, "
+            + "s.name AS student_name "
+            + "FROM course c "
+            + "JOIN course_student cs ON c.id = cs.course_id "
+            + "JOIN student s ON cs.student_id = s.id";
 
-    for (Course course : courses) {
-      String studentSql =
-          "SELECT s.id, s.name FROM student s "
-              + "JOIN course_student cs ON s.id = cs.student_id "
-              + "WHERE cs.course_id = ?";
-      List<Student> students = jdbcTemplate.query(studentSql, studentRowMapper, course.getId());
-      course.setStudentSet(new HashSet<>(students));
-    }
-    return courses;
+    return jdbcTemplate.query(sql, courseWithStudentsExtractor);
   }
 
   @Override
-  public void update(Course course) {
-    String sql =
-        "UPDATE course SET name = ?, description = ?, price = ?, duration = ?, start_date = ?, last_update_date = ?, course_status = ?::course_status  WHERE id = ?";
-    jdbcTemplate.update(
-        sql,
-        course.getName(),
-        course.getDescription(),
-        course.getPrice(),
-        course.getDuration(),
-        course.getStartDate(),
-        course.getLastUpdateDate(),
-        course.getCourseStatus().name(),
-        course.getId());
+  public void update(long id, Map<String, Object> updates) {
+    StringBuilder sql = new StringBuilder("UPDATE course SET ");
+    List<Object> params = new ArrayList<>();
+
+    updates.forEach(
+        (field, value) -> {
+          sql.append(field).append(" = ?, ");
+          params.add(value);
+        });
+
+    sql.setLength(sql.length() - 2);
+    sql.append(" WHERE id = ?");
+    params.add(id);
+
+    jdbcTemplate.update(sql.toString(), params.toArray());
   }
 
   @Override
   public void delete(long id) {
-    jdbcTemplate.update("DELETE FROM course WHERE id = ? ", id);
+    String sql = "DELETE FROM course WHERE id = ?";
+    jdbcTemplate.update(sql, id);
+  }
+
+  @Override
+  public void enrollStudentInCourse(long courseId, long studentId) {
+    String sql = "INSERT INTO course_student (course_id, student_id) VALUES (?, ?)";
+    jdbcTemplate.update(sql, courseId, studentId);
+  }
+
+  @Override
+  public void removeStudentFromCourse(long courseId, Long studentId) {
+    String sql = "DELETE FROM course_student WHERE course_id = ? AND student_id = ?";
+    jdbcTemplate.update(sql, courseId, studentId);
+  }
+
+  @Override
+  public void batchEnrollStudents(long courseId, List<Long> studentIds) {
+    String sql = "INSERT INTO course_student (course_id, student_id) VALUES (?, ?)";
+    jdbcTemplate.batchUpdate(
+        sql,
+        studentIds,
+        studentIds.size(),
+        (ps, studentId) -> {
+          ps.setLong(1, courseId);
+          ps.setLong(2, studentId);
+        });
+  }
+
+  @Override
+  public void batchRemoveStudents(long courseId, List<Long> studentIds) {
+    String sql = "DELETE FROM course_student WHERE course_id = ? AND student_id = ?";
+    jdbcTemplate.batchUpdate(
+        sql,
+        studentIds,
+        studentIds.size(),
+        (ps, studentId) -> {
+          ps.setLong(1, courseId);
+          ps.setLong(2, studentId);
+        });
   }
 }
