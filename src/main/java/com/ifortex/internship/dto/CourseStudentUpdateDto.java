8d2d0f38ffa36815f9eb16a104e8ff1891dcaa5e
AndreySemeynikov <andrey.semeynikov@ifortex.com>
2024-11-15 02:47:43 +0300

    EPS-15: remove unnecessary annotation

--- src/main/java/com/ifortex/internship/dto/CourseStudentUpdateDto.java
+++ src/main/java/com/ifortex/internship/dto/CourseStudentUpdateDto.java
@@ -1,19 +1,16 @@
 package com.ifortex.internship.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourseStudentUpdateDto {
 
   @JsonProperty("courseId")
