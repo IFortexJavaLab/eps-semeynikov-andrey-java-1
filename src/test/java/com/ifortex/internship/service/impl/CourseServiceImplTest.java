e38c17d720b5dc59d20a8735ae2c0db23e4005c1
AndreySemeynikovIF <andrey.semeynikov@ifortex.com>
2024-11-20 16:09:31 +0300

    EPS-7: optimize imports

--- src/test/java/com/ifortex/internship/service/impl/CourseServiceImplTest.java
+++ src/test/java/com/ifortex/internship/service/impl/CourseServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.ifortex.internship.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyMap;
